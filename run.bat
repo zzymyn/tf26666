9965ce5b70953724bcd8d79bbf0b095122a8b4e0
Ivan Neeson <zzymyn@gmail.com>
2017-09-11 22:38:27 +1000

    Disable hltv.

--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-start /MAX server\srcds.exe -console -game tf -port 28000 +clientport 28001 -strictportbind +ip 0.0.0.0 +exec server_init.cfg
+start /MAX server\srcds.exe -console -game tf -port 28000 +clientport 28001 -nohltv -strictportbind +ip 0.0.0.0 +exec server_init.cfg
